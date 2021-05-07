67eed99f9e4d03b7f21e65507fb78866b7ff969e
Amesh Alex Kuruvilla <amesh@Ameshs-Mac-mini.local>
2021-05-07 11:08:13 -0500

    wip

--- App.js
+++ App.js
@@ -1,44 +1,38 @@
 import React from 'react';
-import {
-  StyleSheet,
-  Button,
-  View
-} from 'react-native';
-import ImagePicker from 'react-native-image-picker'
+import {StyleSheet, Button, View} from 'react-native';
+import ImagePicker from 'react-native-image-picker';
 
 const App = () => {
   const pickImage = () => {
     const options = {
       title: 'Select Avatar',
-      customButtons: [{ name: 'fb', title: 'Choose from Facebook' }],
+      customButtons: [{name: 'fb', title: 'Choose from Facebook'}],
       storageOptions: {
         skipBackup: true,
-        path: 'images'
+        path: 'images',
       },
     };
 
-    ImagePicker.showImagePicker(options, (response) => {
+    ImagePicker.showImagePicker(options, response => {
       console.log('Response = ' + response);
 
       if (response.didCancel) {
         console.log('User cancelled image picker');
-
       } else if (response.error) {
         console.log('ImagePicker Error: ', response.error);
       } else if (response.customButton) {
         console.log('User tapped custom button: ', response.customButton);
-
       } else {
         console.log(response.uri);
       }
-
-      
-    })
-  }
+    });
+  };
 
   return (
-    <View>
-      <Button title="Take Image" onPress={pickImage}>hi</Button> 
+    <View style={styles.screen}>
+      <Button title="Take Imagesssss" onPress={pickImage}>
+        hi
+      </Button>
     </View>
   );
 };
@@ -48,6 +42,8 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: 'red',
+    paddingTop: 300,
   },
 });
 
